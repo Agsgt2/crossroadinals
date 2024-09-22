e2e6d7e1ed0f388923f346ac73dafc990cb804a9
Agsgt2 <angelthefourth20242026@gmail.com>
2024-09-22 01:48:33 -0600

    Create crossroadinals.py

---
+++ crossroadinals.py
@@ -0,0 +1,14 @@
+# Scripts
+
+def crosstart(PID):
+  SPID = ''
+  if (SPID !== ''):
+    print("Replacing PID #",SPID,"to PID #",PID)
+    SPID = PID
+  else:
+    if (PID == SPID):
+      print("PID is not changed due to a PID problem")
+    else:
+      print("PID placed to PID #",PID)
+      SPID = PID
+
